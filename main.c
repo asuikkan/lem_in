8a7ff288d0b6d5d058cf9a7f2552f66a11e31319
Arttu Suikkanen <asuikkan@c3r3p5.hive.fi>
2022-06-30 17:37:52 +0300

    progressing with data parsing

--- main.c
+++ main.c
@@ -12,19 +12,83 @@
 
 #include "lem_in.h"
 
+static t_info	*create_info(void)
+{
+	t_info	*info;
+
+	info = (t_info *)malloc(sizeof(t_info));
+	if (!info)
+		return (NULL);
+	info->head = NULL;
+	info->ant_count = 0;
+	return (info);
+}
+
+static void	save_info(char *data)
+{
+	static int		ant_flag;
+	static int		room_flag;
+	static t_room	*table[128];
+	t_info			*info;
+
+	if (ant_flag == 0)
+	{
+		info = create_info();
+		info->ant_count = check_ant_count(data);
+		ant_flag = 1;
+	}
+}
+
+static int	line_len(char *buf)
+{
+	int	i;
+	int	len;
+
+	len = 0;
+	i = -1;
+	while (buf[++i] && buf[i] != '\n')
+		len++;
+	return (len);
+}
+
+static int	save_line(char *buf, char **line)
+{
+	int		len;
+	int		i;
+
+	len = line_len(buf);
+	*line = (char *)malloc(sizeof(char) * (len + 1));
+	if (!*line)
+		return (0);
+	*line[len] = '\0';
+	i = -1;
+	while (++i < len)
+		*line[i] = buf[i];
+	return (len);
+}
+
 static void	read_output(void)
 {
-	int		ret;
-	char	buf[1024 + 1];
+	static char	buf[BUF_SIZE + 1];
+	static char	*line;
+	int			i;
+	int			ret;
 
 	while (1)
 	{
-		ret = read(0, buf, 1024);
+		ret = read(0, buf, BUF_SIZE);
 		if (ret == 0)
 			return ;
 		if (ret < 0)
 			return ;
 		buf[ret] = '\0';
+		i = 0;
+		while (buf[i])
+		{
+			i = save_line(buf, &line);
+			save_info(line);
+			ft_strdel(&line);
+		}
 	}
 }
 
