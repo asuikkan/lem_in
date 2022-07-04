3f10e757b563553aefff3ea539375e3124ac879d
Arttu Suikkanen <asuikkan@c3r4p4.hive.fi>
2022-07-04 16:10:12 +0300

    added dynamic vector functions

---
+++ libft/vec_free.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vec_free.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asuikkan <asuikkan@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 15:02:36 by asuikkan          #+#    #+#             */
+/*   Updated: 2022/07/04 15:02:38 by asuikkan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vec.h"
+
+void	vec_free(t_vec *src)
+{
+	if (!src)
+		return ;
+	free(src->memory);
+	src->memory = NULL;
+	src->alloc_size = 0;
+	src->elem_size = 0;
+	src->len = 0;
+}
