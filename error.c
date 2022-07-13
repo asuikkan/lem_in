be5a075f953b700f69ee77521adba3fe24016df0
Arttu Suikkanen <asuikkan@c3r4p4.hive.fi>
2022-07-13 15:32:09 +0300

    continued with parsing

--- error.c
+++ error.c
@@ -20,5 +20,18 @@ void	error_handler(void)
 
 void	free_info(t_info *info)
 {
+	/*size_t	i;
+	size_t	j;
+	t_room	*temp;
+
+	i = 0;
+	while (i < info->room_table->len)
+	{
+		temp = vec_get(info->room_table, i);
+		j = 0;
+		while (temp->links[j])
+			ft_strdel(&temp->links[j++]);
+		i++;
+	}*/
 	vec_free(info->room_table);
 }
--- reader.c
+++ reader.c
@@ -12,36 +12,59 @@
 
 #include "lem_in.h"
 
+static int	rooms_read(t_info *info, char *line)
+{
+	if (!info->room_table)
+		return (0);
+	if (!ft_strchr(line, ' '))
+		return (1);
+	return (0);
+}
+
+static int	comment_check(t_info *info, char *line)
+{
+	if (ft_strcmp(line, "##start\n") == 0)
+	{
+		if (info->start_flag)
+			return (-1);
+		info->start_flag = 1;
+	}
+	else if (ft_strcmp(line, "##end\n") == 0)
+	{
+		if (info->end_flag)
+			return (-1);
+		info->end_flag = 1;
+	}
+	if ((info->start_flag || info->end_flag) && info->ant_count < 0)
+		return (-1);
+	return (1);
+}
+
 static int	parse_info(t_info *info, char *line)
 {
 	static int	room_flag;
-	static int	start_flag;
-	static int	end_flag;
+	int			ret;
 
+	ret = 1;
 	if (line[0] == '#')
-	{
-		if (ft_strcmp(line, "##start\n") == 0)
-			start_flag = 1;
-		else if (ft_strcmp(line, "##end\n") == 0)
-			end_flag = 1;
-		if ((start_flag || end_flag) && (info->ant_count < 0 || room_flag == 1))
-			return (-1);
-	}
+		ret = comment_check(info, line);
 	else if (info->ant_count < 0)
-	{
-		info->ant_count = parse_ant_count(line); // tee tama paremmin!
-		return (info->ant_count);
-	}
+		ret = parse_ant_count(info, line);
 	else if (room_flag == 0)
-		return (parse_room(info, line, start_flag, end_flag));
-	else
-		return (1);
+	{
+		ret = parse_room(info, line);
+		if (ret == -1 && rooms_read(info, line) == 1)
+			room_flag = 1;
+	}
+	if (room_flag == 1 && line[0] != '#')
+		ret = parse_link(info, line);
+	return (ret);
 }
 
 static int	save_line(char *buf, t_info *info)
 {
-	static int	start;
 	int			len;
+	static int	start;
 	static char	*line;
 
 	while (buf[start])
@@ -50,10 +73,12 @@ static int	save_line(char *buf, t_info *info)
 		if (!line)
 			line = lem_in_strndup(buf, start, len);
 		else
-			line = lem_in_strnjoin(line, buf, start, len);
+			line = lem_in_strnjoin(&line, buf, start, len);
 		if (!line)
 			return (-1);
-		if (line[len - 1] == '\n')
+		if (line[len - 1] != '\n')
+			return (1);
+		else
 		{
 			if (parse_info(info, line) == -1)
 				return (-1);
@@ -68,17 +93,19 @@ static int	save_line(char *buf, t_info *info)
 void	read_output(t_info *info)
 {
 	static char	buf[BUF_SIZE + 1];
+	int			bytes;
 	int			ret;
 
 	while (1)
 	{
-		ret = read(0, buf, BUF_SIZE);
-		if (ret == 0)
+		bytes = read(0, buf, BUF_SIZE);
+		if (bytes == 0)
 			return ;
-		if (ret < 0)
+		if (bytes < 0)
 			return ;
-		buf[ret] = '\0';
-		if (save_line(buf, info) == -1)
+		buf[bytes] = '\0';
+		ret = save_line(buf, info);
+		if (ret == -1)
 		{
 			free_info(info);
 			error_handler();
