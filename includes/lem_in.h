245af14de1fd26d3d38158cbe244c6531e20a84e
Arttu Suikkanen <asuikkan@c3r4p2.hive.fi>
2022-07-14 13:12:20 +0300

    hashing almost done

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -24,12 +24,14 @@ typedef struct s_room
 	int				x;
 	int				y;
 	char			**links;
+	struct s_room	*next;
 }					t_room;
 
 typedef struct s_info
 {
 	int		ant_count;
 	t_vec	*room_table;
+	t_vec	*hash_table;
 	char	*start;
 	char	*end;
 	int		start_flag;
@@ -40,6 +42,7 @@ char	*lem_in_strndup(char *buf, int start, int n);
 char	*lem_in_strnjoin(char *line, char *buf, int start, int n);
 int		add_start(t_info *info, t_room *room);
 int		add_end(t_info *info, t_room *room);
+int		hasher(t_info *info);
 int		lem_in_line_len(char *buf, int start);
 int		parse_ant_count(t_info *info, char *data);
 int		parse_link(t_info *info, char *line);
