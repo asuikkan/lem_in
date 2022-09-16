4486452b7bf84e36667e896c58f729e12a593635
Arttu Suikkanen <arttu.suikkanen@hotmail.com>
2022-09-16 12:01:13 +0300

    pathfinder implementation ongoing

--- pathfinder.c
+++ pathfinder.c
@@ -14,9 +14,6 @@
 
 static int	initialize_bfs_path(t_info *info)
 {
-	info->bfs_path.queue = llist_new(&info->start->matrix_index, sizeof(int));
-	if (!info->bfs_path.queue)
-		return (-1);
 	info->bfs_path.visited = ft_memalloc(sizeof(int) * info->room_count);
 	if (!info->bfs_path.visited)
 		return (-1);
@@ -29,9 +26,47 @@ static int	initialize_bfs_path(t_info *info)
 	return (1);
 }
 
+static int	check_adjacent(t_info *info) // jatka taalta!
+{
+	t_room	*temp;
+	int		i;
+
+	i = -1;
+	while (++i < info->room_count)
+	{
+		if (info->adj_matrix[*info->bfs_path.index][i] == 1
+			&& !info->bfs_path.visited[i])
+		{
+			if (llist_push_back(
+					&info->bfs_path.queue,
+					&i,
+					sizeof(int)) == -1)
+				return (-1);
+		}
+	}
+}
+
+static int	bfs_path(t_info *info, int start)
+{
+	info->bfs_path.visited[start] = 1;
+	llist_push_back(&info->bfs_path.queue, &start, sizeof(int));
+	while (info->bfs_path.queue)
+	{
+		llist_copy_front(
+			info->bfs_path.index,
+			info->bfs_path.queue,
+			sizeof(int));
+		llist_pop(&info->bfs_path.queue);
+		if (check_adjacent(info) == -1)
+			return (-1);
+	}
+}
+
 int	pathfinder(t_info *info)
 {
 	if (initialize_bfs_path(info) == -1)
+		return (-1);
+	if (bfs_path(info, info->start->matrix_index) == -1)
 		return (-1);
 	return (1);
 }
