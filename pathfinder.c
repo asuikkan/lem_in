591472821b1a24eca50cb6c1da3b4acb1ad133db
Arttu Suikkanen <arttu.suikkanen@hotmail.com>
2022-09-16 12:01:12 +0300

    working on algo

--- pathfinder.c
+++ pathfinder.c
@@ -12,8 +12,74 @@
 
 #include "lem_in.h"
 
-int	find_paths(t_info *info)
+static t_room	*find_best_room(t_room *room, int **adj_matrix, int size) // jatka taalta!!
 {
-	a_star(&info->room_table, info->adj_matrix);
+	int	i;
+	int	
+
+	i = -1;
+	while (++i < size)
+	{
+		
+	}
+}
+
+static int	shortest_path(t_info *info, t_vec *path, int *closed_list)
+{
+	t_room	*current;
+	t_room	*next;
+
+	current = info->start;
+	while (current != info->end)
+	{
+		if (vec_push(path, current) == -1)
+			return (-1);
+		next = find_best_room(current, info->adj_matrix, info->room_count);
+		if (!next)
+		{
+			vec_free(path);
+			path = NULL;
+			return (0);
+		}
+	}
+	return (1);
+}
+
+static int	form_path(t_info *info, t_vec *path, int *closed_list)
+{
+	if (vec_new(path, 2, sizeof(t_room *)) == -1)
+		return (-1);
+	if (shortest_path(info, path, closed_list) == -1)
+		return (-1);
+	if (!path)
+	{
+		vec_free(path);
+		path = NULL;
+		return (0);
+	}
+	return (1);
+}
+
+int	pathfinder(t_info *info)
+{
+	t_lists lists;
+	t_vec	*path;
+	t_vec	*paths;
+	int		found_path;
+
+	lists.closed = ft_memalloc(sizeof(int) * info->room_table.len);
+	if (!lists.closed)
+		return (-1);
+	lists.open = ft_memalloc(sizeof(int) * info->room_table.len);
+	if (!lists.open)
+		return (-1);
+	found_path = form_path(info, path, lists.closed);
+	if (found_path == -1)
+		return (-1);
+	if (found_path == 1)
+	{
+		if (vec_push(paths, path) == -1)
+			return (-1);
+	}
 	return (1);
 }
