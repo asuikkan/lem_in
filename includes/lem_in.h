607699211983d749a6a0a48ef3b30d9f37154a4a
Arttu Suikkanen <arttu.suikkanen@hotmail.com>
2022-09-16 12:01:11 +0300

    continued with room data parsing

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -29,13 +29,18 @@ typedef struct s_info
 {
 	int		ant_count;
 	t_vec	*room_table;
+	char	*start;
+	char	*end;
 }			t_info;
 
 char	*lem_in_strndup(char *buf, int start, int n);
 char	*lem_in_strnjoin(char *line, char *buf, int start, int n);
+int		add_start(t_info *info, t_room *room);
+int		add_start(t_info *info, t_room *room);
 int		lem_in_line_len(char *buf, int start);
 int		parse_ant_count(char *data);
-int		parse_room(t_info *info, char *line);
+int		parse_room(t_info *info, char *line, int start_flag, int end_flag);
+int		push_room(t_info *info, t_room *room);
 void	error_handler(void);
 void	free_info(t_info *info);
 void	read_output(t_info *info);
